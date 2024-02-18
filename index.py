b6f27f328db0b2ae02b0fb342e92fbaa3287c5c4
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-17 23:07:01 -0500

    Update index.py

--- index.py
+++ index.py
@@ -4,3 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
+# Definición del modelo de usuario
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
